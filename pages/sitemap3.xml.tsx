d9ce2ae1348a6e90bb915496085074ba79765963
hanyoungjae <han6318@naver.com>
2024-12-27 01:44:39 +0900

    3

--- pages/sitemap3.xml.tsx
+++ pages/sitemap3.xml.tsx
@@ -8,18 +8,15 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     { path: '/path2', date: '2024-10-12' },
   ]
   const contents = sites
-    .map(
-      ({ path, date }) => `
-      <url>
-        <loc>${HOST}${path}</loc>
-        <lastmod>${date}</lastmod>
-      </url>`.trim(),
-    )
+    .map(({ path, date }) => {
+      // 템플릿 리터럴 내에서 불필요한 공백과 개행을 최소화
+      return `<url><loc>${HOST}${path}</loc><lastmod>${date}</lastmod></url>`;
+    })
     .join('');
+
+  // XML 전체 구조 생성
   const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${contents}
-    </urlset>`.trim();
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${contents}</urlset>`;
 
   res.setHeader('Content-Type', 'application/xml');
   res.write(sitemapIndex);
