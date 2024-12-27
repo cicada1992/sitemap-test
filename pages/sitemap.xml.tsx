766a7f2164a72efd7bc02584089effa0dd1eaeac
hanyoungjae <han6318@naver.com>
2024-12-27 13:49:10 +0900

    fix

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -5,24 +5,17 @@ const HOST = "https://sitemap-yj.vercel.app";
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const sites = [
     { path: "/path1", date: "2024-10-10" },
-    { path: "/path2", date: "2024-10-12" },
+    // { path: "/path2", date: "2024-10-12" },
   ];
   console.log(sites);
 
   const contents = []
-    .map(
-      ({ path, date }) => `
-      <sitemap>
-        <loc>${HOST}${path}</loc>
-        <lastmod>${date}</lastmod>
-      </sitemap>
-    `
+    .map(({ path, date }) =>
+      `<sitemap><loc>${HOST}${path}</loc><lastmod>${date}</lastmod></sitemap>`.trim()
     )
     .join("");
-  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
-    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${contents}
-    </sitemapindex>`;
+  const sitemapIndex =
+    `<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${contents}</sitemapindex>`.trim();
 
   res.setHeader("Content-Type", "application/xml");
   res.write(sitemapIndex);
