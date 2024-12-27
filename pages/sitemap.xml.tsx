b0fc70f12e23db743b73d51b1178a24a7badac44
hanyoungjae <han6318@naver.com>
2024-12-27 12:42:10 +0900

    fix

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -7,6 +7,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     { path: "/path1", date: "2024-10-10" },
     { path: "/path2", date: "2024-10-12" },
   ];
+  console.log(sites);
 
   const contents = []
     .map(
