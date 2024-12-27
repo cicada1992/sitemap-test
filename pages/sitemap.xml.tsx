6762cb42e6222999df9858fae967c9e59cef1b06
hanyoungjae <han6318@naver.com>
2024-12-27 12:27:30 +0900

    fix

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,33 +1,34 @@
 import { GetServerSideProps } from "next";
 
-const HOST = 'https://sitemap-yj.vercel.app'
+const HOST = "https://sitemap-yj.vercel.app";
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-    const sites = [
-        { path: '/path1', date: '2024-10-10' },
-        { path: '/path2', date: '2024-10-12' },
-    ]
-    const contents = sites
-        .map(
-            ({ path, date }) => `
-      <url>
+  const sites = [
+    { path: "/path1", date: "2024-10-10" },
+    { path: "/path2", date: "2024-10-12" },
+  ];
+
+  const contents = sites
+    .map(
+      ({ path, date }) => `
+      <sitemap>
         <loc>${HOST}${path}</loc>
         <lastmod>${date}</lastmod>
-      </url>`,
-        )
-        .join('');
-    const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      </sitemap>`
+    )
+    .join("");
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${contents}
-</urlset>`;
+</sitemapindex>`;
 
-    res.setHeader('Content-Type', 'application/xml');
-    res.write(sitemapIndex);
-    res.end();
+  res.setHeader("Content-Type", "application/xml");
+  res.write(sitemapIndex);
+  res.end();
 
-    return { props: {} };
-}
+  return { props: {} };
+};
 
 export default function SitemapIndex() {
-    return null;
+  return null;
 }
