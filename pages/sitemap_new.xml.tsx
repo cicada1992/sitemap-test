a7bdbc5ceac701920547938638451c6a365db174
hanyoungjae <han6318@naver.com>
2024-12-29 22:25:00 +0900

    fix sitemap_new

--- pages/sitemap_new.xml.tsx
+++ pages/sitemap_new.xml.tsx
@@ -4,7 +4,7 @@ const HOST = "https://sitemap-yj.vercel.app";
 
 const insideXMLString = (xmlContent: string): string => {
   return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
       ${xmlContent}
     </urlset>
   `;
@@ -25,7 +25,6 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     `;
   }
 
-  // 기본 XML 템플릿 사이에 데이터들을 담은 XML 내용을 넣어줍시다.
   const xmlContents = insideXMLString(pagesXML);
 
   res.setHeader("Content-Type", "application/xml");
