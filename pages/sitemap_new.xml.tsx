52efe2165f1196af45706760702b7624346f9ff1
hanyoungjae <han6318@naver.com>
2024-12-30 09:38:00 +0900

    fix error

--- pages/sitemap_new.xml.tsx
+++ pages/sitemap_new.xml.tsx
@@ -4,7 +4,7 @@ const HOST = "https://sitemap-yj.vercel.app";
 
 const insideXMLString = (xmlContent: string): string => {
   return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${xmlContent}
     </urlset>
   `;
