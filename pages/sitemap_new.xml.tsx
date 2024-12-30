12c828e537a7d78c527937504cc1b7b8781b02a5
hanyoungjae <han6318@naver.com>
2024-12-30 10:49:16 +0900

    chore sitemap_new

--- pages/sitemap_new.xml.tsx
+++ pages/sitemap_new.xml.tsx
@@ -2,10 +2,10 @@ import { GetServerSideProps } from "next";
 
 const HOST = "https://sitemap-yj.vercel.app";
 
-const insideXMLString = (xmlContent: string): string => {
+const getXMLContents = (xmlContent: string): string => {
   return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${xmlContent}
+      ${xmlContent.trim()}
     </urlset>
   `;
 };
@@ -25,8 +25,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     `;
   }
 
-  const xmlContents = insideXMLString(pagesXML);
-
+  const xmlContents = getXMLContents(pagesXML);
   res.setHeader("Content-Type", "application/xml");
   res.write(xmlContents);
   res.end();
